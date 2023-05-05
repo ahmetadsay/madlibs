54f053b4a993d516dfb6af99f738540ed517682b
daliakhalifa02 <daliakhalifa02@gmail.com>
2023-05-06 01:43:03 +0300

    Changed story and added design

--- madlibs.js
+++ madlibs.js
@@ -1,47 +1,48 @@
-/**
- * Complete the implementation of parseStory.
- *
- * parseStory retrieves the story as a single string from story.txt
- * (I have written this part for you).
- *
- * In your code, you are required (please read this carefully):
- * - to return a list of objects
- * - each object should definitely have a field, `word`
- * - each object should maybe have a field, `pos` (part of speech)
- *
- * So for example, the return value of this for the example story.txt
- * will be an object that looks like so (note the comma! periods should
- * be handled in the same way).
- *
- * Input: "Louis[n] went[v] to the store[n], and it was fun[a]."
- * Output: [
- *  { word: "Louis", pos: "noun" },
- *  { word: "went", pos: "verb", },
- *  { word: "to", },
- *  { word: "the", },
- *  { word: "store", pos: "noun" }
- *  { word: "," }
- *  ....
- *
- * There are multiple ways to do this, but you may want to use regular expressions.
- * Please go through this lesson: https://www.freecodecamp.org/learn/javascript-algorithms-and-data-structures/regular-expressions/
- */
 function parseStory(rawStory) {
-  // Your code here.
-  return {}; // This line is currently wrong :)
+  const story = document.querySelector(".madLibsEdit");
+  const deleteWord = rawStory.replace(/\b\w+\[/g, "[");
+  const putInputs = deleteWord.replace(
+    /\[n\]/g,
+    "<input id='noun'  placeholder='noun' />"
+  ).replace(
+    /\[v\]/g,
+    "<input id='verb'   placeholder='verb' />"
+  ).replace(
+    /\[a\]/g,
+    "<input id='adjective'  placeholder='adjective' />"
+  );
+
+  const parser = new DOMParser();
+  const html = parser.parseFromString(putInputs, "text/html");
+  story.appendChild(html.body);
+
+  const duplicateStory = document.querySelector(".madLibsPreview");
+  duplicateStory.innerHTML = putInputs.replace(/<input[^>]*>/g, "<span></span>");
+
+  const inputs = document.querySelectorAll(".madLibsEdit input");
+  const spans = document.querySelectorAll(".madLibsPreview span");
+
+  inputs.forEach((input, i) => {
+    const span = spans[i];
+
+    input.addEventListener("input", () => {
+      span.textContent = input.value;
+    });
+
+    input.addEventListener("keydown", (event) => {
+      if (event.keyCode === 13) {
+        if (i < inputs.length - 1) {
+          inputs[i + 1].focus();
+        }
+
+        else if ( i = inputs.length ) {
+          inputs[0].focus()
+        }
+      }
+    });
+  });
 }
 
-/**
- * All your other JavaScript code goes here, inside the function. Don't worry about
- * the `then` and `async` syntax for now.
- *
- * NOTE: You should not be writing any code in the global namespace EXCEPT
- * declaring functions. All code should either:
- * 1. Be in a function.
- * 2. Be in .then() below.
- *
- * You'll want to use the results of parseStory() to display the story on the page.
- */
 getRawStory()
   .then(parseStory)
   .then((processedStory) => {
